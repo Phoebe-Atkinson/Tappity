fd0598ad4abe604c2dcf417a1b0700d7504aa091
nullUser <nullUser@40955.local>
2017-05-29 11:40:53 +1000

    Planning & basic code implementation

--- Tappity/ViewController.swift
+++ Tappity/ViewController.swift
@@ -13,12 +13,40 @@ class ViewController: UIViewController {
     @IBOutlet var timeLabel: UILabel!
     
     @IBOutlet var scoreLabel: UILabel!
+    
+    
+    var isPlaying: Bool = false
+    var presses: Int = 0
+    
 
-    @IBAction func mainButtonPushed() {
+    @IBAction func mainButtonPushed(_ sender: UIButton) {
         
         
+        if (!isPlaying) {
+            
+            
+            startGame()
+            return
+            
+        }
         
         
+        //    if isPlaying
+        // vv              vv
+        //
+        
+        // increment the presses var once
+        
+        
+    }
+    
+    func startGame() {
+        
+        isPlaying = true
+        
+        // begin the game and start the timer
+        
+        presses += 1
         
     }
 
