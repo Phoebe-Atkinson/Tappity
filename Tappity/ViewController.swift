2d33299009fc0b52644fd35ee82698f36261f71d
nullUser <nullUser@40955.local>
2017-05-29 11:27:02 +1000

    Initial Commit

---
+++ Tappity/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tappity
+//
+//  Created by nullUser on 29/5/17.
+//  Copyright © 2017 Roan Atkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var timeLabel: UILabel!
+    
+    @IBOutlet var scoreLabel: UILabel!
+
+    @IBAction func mainButtonPushed() {
+        
+        
+        
+        
+        
+    }
+
+
+}
+
