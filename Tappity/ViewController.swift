dbe5dda72214560773f6da77a1eeeab49a2a4727
nullUser <nullUser@40955.local>
2017-05-29 11:45:35 +1000

    Updated comments

--- Tappity/ViewController.swift
+++ Tappity/ViewController.swift
@@ -10,22 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // the Time display label
     @IBOutlet var timeLabel: UILabel!
     
+    // the Score display label
     @IBOutlet var scoreLabel: UILabel!
     
     
+    // isPlaying boolean var to store whether the user is in a game or not
     var isPlaying: Bool = false
     var presses: Int = 0
     
 
+    // mainButtonPushed func hooked up to the button
     @IBAction func mainButtonPushed(_ sender: UIButton) {
         
-        
+        // run seperate code to execute the game
         if (!isPlaying) {
             
-            
+            // call the startGame func
             startGame()
+            
+            // return so any following code will not be fun
             return
             
         }
@@ -42,10 +48,12 @@ class ViewController: UIViewController {
     
     func startGame() {
         
+        // set isPlaying to true
         isPlaying = true
         
         // begin the game and start the timer
         
+        // increment presses on first go because i'm nice
         presses += 1
         
     }
